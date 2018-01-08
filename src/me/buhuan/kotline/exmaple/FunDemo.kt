8ceed2589f96eab85e196d0dbc103ac743d78e8f
黄步欢 <buhuanhuang@gmail.com>
2018-01-08 21:47:49 +0800

    添加函数demo

---
+++ src/me/buhuan/kotline/exmaple/FunDemo.kt
@@ -0,0 +1,13 @@
+package me.buhuan.kotline.exmaple
+
+//fun sum(a: Int, b: Int): Int {
+//	return a + b
+//}
+fun sum(a: Int, b: Int) = a + b
+
+//fun printSum(a: Int, b: Int): Unit {
+//	println("sum of $a and $b is ${a + b}")
+//}
+fun printSum(a: Int, b: Int) {
+	println("sum of $a and $b is ${a + b}")
+}
