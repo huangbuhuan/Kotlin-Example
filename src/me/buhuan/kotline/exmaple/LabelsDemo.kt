c0f5423bc2f1edbdbdbbb7afcffe7a1a8860d053
黄步欢 <buhuanhuang@gmail.com>
2018-01-13 22:29:34 +0800

    add foo function

--- src/me/buhuan/kotline/exmaple/LabelsDemo.kt
+++ src/me/buhuan/kotline/exmaple/LabelsDemo.kt
@@ -1,9 +1,18 @@
 package me.buhuan.kotline.exmaple
 
+fun foo() {
+    val ints = arrayOf(1, 2, 3, 0,  4)
+    ints.forEach lit@{
+        if (it == 0) return@lit
+        println(it)
+    }
+}
+
 fun main(args: Array<String>) {
     loop@ for(i in 1..100) {
         for(j in 1..100) {
             if(j > 1) break@loop
         }
     }
+    foo()
 }
