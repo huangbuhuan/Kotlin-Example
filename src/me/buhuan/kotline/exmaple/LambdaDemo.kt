313c4198717d2aaeb4939eff6eff4e0e4154c44b
黄步欢 <buhuanhuang@gmail.com>
2018-01-14 22:57:20 +0800

    add lombda demo

--- src/me/buhuan/kotline/exmaple/LambdaDemo.kt
+++ src/me/buhuan/kotline/exmaple/LambdaDemo.kt
@@ -1,5 +1,19 @@
 package me.buhuan.kotline.exmaple
 
+import java.util.concurrent.locks.Lock
+
+fun <T> lock(lock: Lock, body: () -> T): T {
+    lock.lock()
+    try {
+        return body()
+    }
+    finally {
+        lock.unlock()
+    }
+}
+
+
+
 fun main(args: Array<String>) {
 	val list = arrayOf(1, 2, 3, 4)
 	var sum = list.filter { it > 3 }.sum()
